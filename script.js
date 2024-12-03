28bc6a1ca44de05f32d8a1844fbc93d8763377d5
Ali Ekmekci <aliosmanekmekci@gmail.com>
2024-12-03 21:23:45 +0300

    wipe the comments for rearrange

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
-//Bunun icine random yerlestirecegiz. o da tas, kagit veya makastan bi tanesini sececek.
-//ama o randomu nasil atayacagiz, o da var
 let humanScore = 0;
 let computerScore = 0;
 
 function getComputerChoice() {}
-//ayni zamanda biz de oynayacagiz o yuzden insan secimi de girilmeli onun icin de bir fonksiyon yaz
+
 function getHumanChoice() {
   let humanChoice = prompt("Enter your choice human! Rock, paper & scissors?");
   switch (humanChoice) {
     case "Rock":
-      humanChoice = "Rock";
       break;
     case "Paper":
-      humanChoice = "Paper";
       break;
     case "Scissors":
-      humanChoice = "Scissors";
       break;
     default:
-       humanChoice = "Give me a value dude!";
+      console.error("Give me a proper hand dude!This doesn't count!");
   }
-    console.log(humanChoice)
-    return humanChoice;
+
+  return humanChoice;
 }
 
-getHumanChoice()
+console.log(getHumanChoice());
+
 //yani biz simdi playRound'i 3 kere calistiracagiz
 function playRound(humanChoice, ComputerChoice) {}
