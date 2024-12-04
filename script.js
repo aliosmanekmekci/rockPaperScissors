ae69c0f6765a7d4c1d84cf53e1119ced0d5b480f
Ali Ekmekci <aliosmanekmekci@gmail.com>
2024-12-05 01:15:38 +0300

    Implemented initial version of Rock Paper Scissors game.

--- script.js
+++ script.js
@@ -5,6 +5,33 @@ function getComputerChoice() {
   const options = ["rock", "paper", "scissors"];
   return options[Math.floor(Math.random() * 3)];
 }
-function getHumanChoice() {}
-function playRound() {}
-function playGame() {}
+function getHumanChoice() {
+  let humanInput = prompt("Rock, paper or scissors?");
+  return humanInput.toLowerCase();
+}
+function playRound() {
+  const humanHand = getHumanChoice();
+  const computerHand = getComputerChoice();
+  if (humanHand === computerHand) {
+    console.log(`It's a tie!`);
+  } else if (
+    (humanHand === "rock" && computerHand === "scissors") ||
+    (humanHand === "paper" && computerHand === "rock") ||
+    (humanHand === "scissors" && computerHand === "paper")
+  ) {
+    console.log(`Human win! ${humanHand} beats ${computerHand}`);
+    humanScore++;
+  } else {
+    console.log(`Human win! ${computerHand} beats ${humanHand}`);
+    computerScore++;
+  }
+}
+
+function playGame() {
+  for (let i = 1; i <= 3; i++) {
+    playRound();
+  }
+  console.log(`computer: ${computerScore} & human: ${humanScore}`);
+}
+
+playGame();
